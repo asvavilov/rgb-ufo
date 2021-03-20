be28ae6d100d83d668a4ec2ee94101dfd2014526
asvavilov <vavilov.rf@gmail.com>
2021-03-20 15:06:41 +0300

    - оставлена только fastled - 2 ленты (6 внутренних и 24 внешних пикселя) - hue

--- src/main.cpp
+++ src/main.cpp
@@ -4,112 +4,83 @@
 
 // TODO навести порядок (убрать мусор, определиться с библиотекой)
 
-// adafruit (default) or fastled
-#define FASTLED
-
 // NeoPixel Ring simple sketch (c) 2013 Shae Erisson
 // released under the GPLv3 license to match the rest of the AdaFruit NeoPixel library
 
-#ifdef FASTLED
 #include "FastLED.h"
-#else
-#include <Adafruit_NeoPixel.h>
-#endif
 #ifdef __AVR__
 #include <avr/power.h>
 #endif
 
-// Which pin on the Arduino is connected to the NeoPixels?
-// On a Trinket or Gemma we suggest changing this to 1
-#define PIN            3
+#define OUTER_LED_PIN 3
+#define INNER_LED_PIN 5
 
-// How many NeoPixels are attached to the Arduino?
-#define NUMPIXELS      6
+#define OUTER_LED_NUM 24
+#define INNER_LED_NUM 6
 
-#ifdef FASTLED
-CRGB leds[NUMPIXELS];
-#endif
+CRGB innerLeds[INNER_LED_NUM];
+CRGB outerLeds[OUTER_LED_NUM];
 
-// When we setup the NeoPixel library, we tell it how many pixels, and which pin to use to send signals.
-// Note that for older NeoPixel strips you might need to change the third parameter--see the strandtest
-// example for more information on possible values.
-#ifndef FASTLED
-Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
-#endif
+int outerLedDelay = 10; // delay
+unsigned long outerLedTimer = 0;
+int innerLedDelay = 500; // delay
+unsigned long innerLedTimer = 0;
 
-int delayval = 100; // delay
-char* mode = "rgb"; // random|step|rgb
-int r = 0;
-int g = 0;
-int b = 0;
-int cstep = 20;
 int rgbCnt = 0;
+uint8_t gHue = 0;
+uint8_t gHueDelta = 3;
 
 void setup()
 {
-	// This is for Trinket 5V 16MHz, you can remove these three lines if you are not using a Trinket
-#if defined (__AVR_ATtiny85__)
-	if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
-#endif
-	// End of trinket special code
-#ifdef FASTLED
-	FastLED.addLeds<NEOPIXEL, PIN>(leds, NUMPIXELS);
-#else
-	pixels.begin(); // This initializes the NeoPixel library.
-#endif
+	FastLED.addLeds<NEOPIXEL, OUTER_LED_PIN>(outerLeds, OUTER_LED_NUM);
+	FastLED.addLeds<NEOPIXEL, INNER_LED_PIN>(innerLeds, INNER_LED_NUM);
+
 	randomSeed(analogRead(0));
 }
 
 void loop()
 {
-	for (int i = 0; i < NUMPIXELS; i++)
+
+	// TODO см. управление цветами https://github.com/FastLED/FastLED/wiki/Controlling-leds
+
+	unsigned long ms = millis();
+
+	if (ms > outerLedTimer + outerLedDelay)
 	{
-		if (mode == "random")
-		{
-			r = random(255);
-			g = random(255);
-			b = random(255);
-		}
-		else if (mode == "step")
-		{
-			r += cstep;
-			if (r > 255)
-			{
-				r = 0;
-				g += cstep;
-			}
-			if (g > 255)
-			{
-				g = 0;
-				b += cstep;
-			}
-			if (b > 255)
-			{
-				b = 0;
-			}
-		}
-		else if (mode == "rgb")
+		outerLedTimer = ms;
+
+		/*for (int i = 0; i < OUTER_LED_NUM; i++)
 		{
 			r = (rgbCnt + i) % 3 == 0 ? 255 : 0;
 			g = (rgbCnt + i) % 3 == 1 ? 255 : 0;
 			b = (rgbCnt + i) % 3 == 2 ? 255 : 0;
+			outerLeds[i] = CRGB(r, g, b);
+		}*/
+		/*// так иногда красный пиксель по всем по кругу пробегает
+		gHue += gHueDelta;
+		fill_rainbow(outerLeds, OUTER_LED_NUM, gHue, gHueDelta);*/
+		gHue += gHueDelta;
+		for (int i = 0; i < OUTER_LED_NUM; i++)
+		{
+			outerLeds[i].setHue((gHue + i * 10) % 255);
 		}
-#ifdef FASTLED
-		leds[i] = CRGB(r, g, b);
-#else
-		// pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
-		pixels.setPixelColor(i, pixels.Color(r, g, b));
-#endif
 	}
-#ifdef FASTLED
-	FastLED.show();
-#else
-	pixels.show(); // This sends the updated pixel color to the hardware.
-#endif
-	delay(delayval);
-	rgbCnt++;
-	if (rgbCnt >= 3)
+
+	if (ms > innerLedTimer + innerLedDelay)
 	{
-		rgbCnt = 0;
+		innerLedTimer = ms;
+
+		int r = (rgbCnt) % 3 == 2 ? 255 : 0;
+		int g = (rgbCnt) % 3 == 1 ? 255 : 0;
+		int b = (rgbCnt) % 3 == 0 ? 255 : 0;
+		fill_solid(innerLeds, INNER_LED_NUM, CRGB(r, g, b));
+		rgbCnt++;
+		if (rgbCnt >= 3)
+		{
+			rgbCnt = 0;
+		}
 	}
+
+	FastLED.show();
+	delay(10);
 }
